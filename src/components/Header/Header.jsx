54a92b0d2cbc49116414b7ec69b68c4c0df6e7c6
Atharv Bharadwaj <96018989+0019atharv@users.noreply.github.com>
2025-08-26 20:06:37 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header-contents">
-        <h3>Khaana denge aisa,aap bina maange doge paisa! 😋😋</h3>
+        <h3>Khaana denge aisa,aap bina maange doge paisa!😋</h3>
         <p>
           Order Your Favourite Food from the Best Kitchen.
         </p>
